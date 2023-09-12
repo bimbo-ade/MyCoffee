fc23f141c37781a6a95804245eec702e6af2a9e9
bimbo-ade <bimboadeyemi65@gmail.com>
2023-09-12 18:23:25 +0100

    bg colors

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,27 +6,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.brown[900],
         body: Stack(
-          children: [
-            Container(
-              color: const Color.fromARGB(255, 63, 22, 8), // First color
-              width: double.infinity,
-              height: double.infinity,
-            ),
-            Container(
-              color: Color.fromARGB(255, 247, 247, 247), // Second color
-              width: double.infinity,
-              height: 240,
-              child: const Center(
-                child: Text(
-                  'Hello, Two-Colored Background!',
-                  style: TextStyle(color: Colors.white, fontSize: 24),
+      children: [
+        Container(
+          color: const Color.fromARGB(255, 63, 22, 8), // First color
+          width: double.infinity,
+          height: double.infinity,
+        ),
+        Container(
+            color: const Color.fromARGB(255, 247, 247, 247), // Second color
+            width: double.infinity,
+            height: 240,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-            ),
-          ],
-        )
+                const CircleAvatar(
+                  backgroundImage: AssetImage('assets/image.jpg'),
+                  radius: 20,
+                )
+              ],
+            )),
+      ],
+    )
 
         // Padding(
         //   padding: const EdgeInsets.all(20.0),
