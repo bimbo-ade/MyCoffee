353a6d14c1dc8eaee9539f0f63598ca6ac82a9d5
bimbo-ade <bimboadeyemi65@gmail.com>
2023-09-13 08:27:42 +0100

    home header

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,61 +6,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          color: const Color.fromARGB(255, 63, 22, 8), // First color
-          width: double.infinity,
-          height: double.infinity,
-        ),
-        Container(
-            color: const Color.fromARGB(255, 247, 247, 247), // Second color
-            width: double.infinity,
-            height: 240,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    color: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              color: const Color(0xFFF9F9F9), // First color
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Color(0xFF131313), Color(0xFF2C2C2C)],
                   ),
                 ),
-                const CircleAvatar(
-                  backgroundImage: AssetImage('assets/image.jpg'),
-                  radius: 20,
-                )
-              ],
-            )),
-      ],
-    )
-
-        // Padding(
-        //   padding: const EdgeInsets.all(20.0),
-        //   child: SafeArea(
-        //     child: Row(
-        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //       children: [
-        //         IconButton(
-        //           onPressed: () {
-        //             Navigator.pop(context);
-        //           },
-        //           icon: const Icon(
-        //             Icons.arrow_back,
-        //             color: Colors.white,
-        //           ),
-        //         ),
-        //         const CircleAvatar(
-        //           backgroundImage: AssetImage('assets/image.jpg'),
-        //           radius: 20,
-        //         )
-        //       ],
-        //     ),
-        //   ),
-        // ),
-        );
+                width: double.infinity,
+                height: 240,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const CircleAvatar(
+                        backgroundImage: AssetImage('assets/image.jpg'),
+                        radius: 20,
+                      )
+                    ],
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
