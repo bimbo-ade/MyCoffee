84f199c3d65ed2f840568d512057b2795d5e6c54
bimbo-ade <bimboadeyemi65@gmail.com>
2023-09-13 09:40:06 +0100

    search field added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  // final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,37 +17,67 @@ class HomeScreen extends StatelessWidget {
               height: double.infinity,
             ),
             Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Color(0xFF131313), Color(0xFF2C2C2C)],
-                  ),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xFF131313), Color(0xFF2C2C2C)],
                 ),
-                width: double.infinity,
-                height: 240,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
+              ),
+              width: double.infinity,
+              height: 240,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      const CircleAvatar(
-                        backgroundImage: AssetImage('assets/image.jpg'),
-                        radius: 20,
-                      )
-                    ],
-                  ),
-                )),
+                        const CircleAvatar(
+                          backgroundImage: AssetImage('assets/image.jpg'),
+                          radius: 20,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20.0),
+                    Column(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)),
+                          child: const TextField(
+                            // controller: _searchController,
+                            decoration: InputDecoration(
+                                labelText: 'Search',
+                                hintText: 'Search...',
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                  size: 20,
+                                ),
+                                border: InputBorder.none),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
