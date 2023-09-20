45017f1c70e4da7f6d48d4fdb2e5a4c090065420
bimbo-ade <bimboadeyemi65@gmail.com>
2023-09-20 08:35:58 +0100

    menu card displayed

--- lib/menu_list.dart
+++ lib/menu_list.dart
@@ -7,11 +7,25 @@ class MenuList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      padding: const EdgeInsets.all(8),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          leading: const Icon(Icons.dangerous),
-          title: Text(items[index]),
+        return Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(
+                255, 61, 61, 61), // Background color of the item
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          margin: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(26.0),
+          child: Text(
+            'Item $index',
+            style: const TextStyle(
+              color: Colors.white, // Text color
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0, // Text size
+            ),
+          ),
         );
       },
     );
