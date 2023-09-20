954be2ec6d74674f1c9bc4251191713a9dbb6df9
bimbo-ade <bimboadeyemi65@gmail.com>
2023-09-20 09:47:07 +0100

    items style

--- lib/menu_list.dart
+++ lib/menu_list.dart
@@ -12,19 +12,36 @@ class MenuList extends StatelessWidget {
       itemBuilder: (context, index) {
         return Container(
           decoration: BoxDecoration(
-            color: const Color.fromARGB(
-                255, 61, 61, 61), // Background color of the item
+            color: const Color.fromARGB(255, 61, 61, 61),
             borderRadius: BorderRadius.circular(5.0),
           ),
           margin: const EdgeInsets.all(8.0),
-          padding: const EdgeInsets.all(26.0),
-          child: Text(
-            'Item $index',
-            style: const TextStyle(
-              color: Colors.white, // Text color
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0, // Text size
-            ),
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('image'),
+              const SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Item $index',
+                    style: const TextStyle(
+                      color: Colors.white, // Text color
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0, // Text size
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text('fghj'),
+                ],
+              ),
+            ],
           ),
         );
       },
